f381b30c631fd08969e0d229a927f50048d4bc05
Assanvo Brou Antoine <assanvobrou45@gmail.com>
2026-04-25 16:07:43 +0000

    fix: super admin password + build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit
-
 pip install -r requirements/prod.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py populate_ddp_data
 python manage.py populate_assurances
 python manage.py repair_permissions
+python manage.py shell -c "
+from apps.authentication.models import User
+u = User.objects.filter(role='super_admin').first()
+if u:
+    u.set_password('admin@ddp2026')
+    u.save()
+    print('Super admin password reset OK')
+"
